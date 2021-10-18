7d77c223fac975bc08e6a632fd38ee8cd89118d7
Aficionado <volodiag@mail.ru>
2021-10-18 15:37:55 +0300

    Added Icon picture. Function changed to switch between red, yellow, green colours. Added button for switching device's torch on and off.

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -6,21 +6,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     var screenColor: Int = 0
     
+    @IBOutlet weak var torchButton: UIButton!
+    
+    var torchButtonTitle = "Off"
+    
+    let device = AVCaptureDevice.default(for: AVMediaType.video)
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
 
+        // MARK: - Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //view.backgroundColor = .black
         updateUI()
     }
 
+    // This func is changing the screen color.
     fileprivate func updateUI() {
         switch screenColor {
         case 0:
@@ -36,7 +45,32 @@ class ViewController: UIViewController {
         screenColor += 1
         
     }
+    // This func is calling the func which switches the torch on&off, and changing the title in torchButton
+    @IBAction func torchButtonPressed(_ sender: Any) {
+        
+        torchOnOff()
+        torchButtonTitle = torchButtonTitle == "Off" ? "On" : "Off"
+        torchButton.setTitle(torchButtonTitle, for: .normal)
+    }
     
+    
+    // Func which switches the torch on&off
+    func torchOnOff() {
+        
+        if ((device?.hasTorch) != nil) {
+            do {
+                try device?.lockForConfiguration()
+                device?.torchMode = device?.torchMode == AVCaptureDevice.TorchMode.on ? .off : .on
+            } catch {
+                print(error)
+            }
+            device?.unlockForConfiguration()
+            
+        }
+        
+    }
+    
+    // When touching the screen, its color is changed: red, yellow, white.
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         updateUI()
     }
